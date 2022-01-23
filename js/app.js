d4441a591dfdb4c4bbcac620a39da08d79393be4
ibrahimalaqoul <alaqoulibrahim@gmail.com>
2022-01-23 18:20:53 +0200

    newjs

--- js/app.js
+++ js/app.js
@@ -15,6 +15,28 @@ else {
     } else if (userGender.toLocaleLowerCase() === "female") 
     {
     alert("Welcome Ms "+ userName);       
-    } else { alert("welcome " + userName);
+    } else { alert("welcome ");
     } 
 }
+let myArray = [];
+let userAnswer1 = prompt("Are you A fiend's fan?");
+let userAnswer2 = prompt("do you like spring?");
+let userAnswer3 = prompt("do you like winter?");
+//function to push user's answers to the array.
+function theUserAnswers(array, answer1) {
+    if (answer1 != "yes" && answer1 != "no") {
+        array.push("invalid");
+    } else {
+        array.push(answer1);
+    }
+}
+theUserAnswers(myArray, userAnswer1);
+theUserAnswers(myArray, userAnswer2);
+theUserAnswers(myArray, userAnswer3);
+//function to print user's answers.
+function theUserAnswersPrinter(array) {
+    for (let i = 0; i < array.length; i++) {
+        console.log(array[i]);
+    }
+}
+theUserAnswersPrinter(myArray);
